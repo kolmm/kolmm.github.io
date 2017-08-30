78721d193853ec1798d368448178e9a7c4fca3fb
kolmm <kolmm3@gmail.com>
2017-08-31 00:59:38 +0500

    add transport

---
+++ Transport/src/js/common.js
@@ -0,0 +1,14 @@
+// $( document ).ready(function() {
+// 	$('.bt-menu').click(function(){
+// 		$('.nav-items').fadeToggle();
+// 	});
+//
+// 	(function($) {
+//
+// 		$('.bt-menu').click(function(e) {
+// 			$(this).toggleClass('.menu-on');
+// 			return false;
+// 		});
+//
+// 	});
+// });
